d07d4d6242232ee89203061b1923637884418151
Jujurkasian <rizkykosong9876@gmail.com>
2026-04-26 20:10:51 +0700

    use env API_KEY

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-const API_KEY = process.env.API_KEY;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({
   origin: '*',
