a61c244e4e9c346d735e0d07d33f0b7a81ca0c19
Jujurkasian <rizkykosong9876@gmail.com>
2026-04-27 12:04:56 +0700

    update

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ const AVDB_TYPES = {
 
 function normalizeItem(item) {
   const code = item.movie_code || item.slug || "";
-  const slug = item.slug || code.toLowerCase();
+  const slug = item.slug || (item.movie_code || "").toLowerCase().replace(/ /g, '-');
   const embedUrl = item.episodes?.server_data?.Full?.link_embed || "";
   
   return {
@@ -73,7 +73,7 @@ function normalizeItem(item) {
     slug:        slug,
     type:        item.type_name || "",
     poster_url:  item.poster_url || `https://upload18.cc/v/${slug}/poster.jpg`,
-    thumb_url:   item.thumb_url  || `https://upload18.cc/v/${slug}/thumb.jpg`,
+    thumb_url:   item.thumb_url || `https://upload18.cc/v/${slug}/thumb.jpg`,
     actors:      (item.actor || []).filter(a => a !== "Updating").join(", "),
     director:    (item.director || []).filter(d => d !== "Updating").join(", "),
     categories:  item.category || [],
